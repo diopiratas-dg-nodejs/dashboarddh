4693024426b97f78067a64873c9399220c835c45
Diogenes Pereira <diogenes.pereira@luizalabs.com>
2022-03-29 11:21:16 -0300

    init: create project

---
+++ src/components/ContentRowTop.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ContentRowMovies from './ContentRowMovies';
+import LastMovieInDb from './LastMovieInDb';
+import GenresInDb from './GenresInDb';
+function ContentRowTop(){
+    return(
+        <React.Fragment>
+				{/*<!-- Content Row Top -->*/}
+				<div className="container-fluid">
+					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
+						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+					</div>
+				
+					{/*<!-- Content Row Movies-->*/}
+					<div className="row">
+						<ContentRowMovies />
+					</div>
+					{/*<!-- End movies in Data Base -->*/}
+					
+	
+					{/*<!-- Content Row Last Movie in Data Base -->*/}
+					<div className="row">
+						<LastMovieInDb />
+
+						<GenresInDb />
+					</div>
+				</div>
+				{/*<!--End Content Row Top-->*/}
+
+        </React.Fragment>
+    )
+
+}
+export default ContentRowTop;
